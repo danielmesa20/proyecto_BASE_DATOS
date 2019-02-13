df0b46012ce8fe86304642c6774cc6d1deb635c6
Daniel Alexander Mesa Esquivel <vznlprogrammer@gmail.con>
2019-02-12 21:32:55 -0400

    Arreglos menores

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname,'public')));
 
 //Index route
-app.get('/', (req,res) => res.render('index', {layout: 'landing'}));
+app.get('/', (req,res) => res.render('paginaPrincipal', {layout: 'landing'}));
 
 //Routes Aviones
 app.use('/aviones', require('./routes/aviones'));
--- config/database.js
+++ config/database.js
@@ -1,19 +1,3 @@
-/*const Sequelize = require('sequelize');
-module. exports = new Sequelize('aeropuerto', 'root', 'password', {
-  host: 'localhost',
-  dialect: 'mysql',
-  operatorsAliases: false,
-
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
-
-});
-*/
-
 const Sequelize = require("sequelize");
 Sequelize.Promise = global.Promise;
 
